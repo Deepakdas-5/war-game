afeb577a566dbe671bc6dcf4e74c266546e593db
DEEPAKDAS <deepakdasedt23@gmail.com>
2026-06-19 07:27:14 +0530

    last logic done

--- war.py
+++ war.py
@@ -52,8 +52,17 @@ class player():
 
            self.player_card.append(deck.deal_one())
 
+    def _fetch(self):
+        return self.player_card.pop(0)
+
+    def _vangichoo_(self,a):
+        self.player_card.extend(a)
+
+    
+
     def __repr__(self):
         return f"player {self.name} : {self.player_card}"
+
            
             
 d=deck()
@@ -64,6 +73,73 @@ print(p1)
 p2=player("two")
 p2.player_cards(d)
 print(p2)
+
+game_on=True
+round=0
+
+while game_on:
+
+    round+=1
+
+    if (len(p1.player_card)==0):
+        print("player one out of card player one lost")
+        print("game over")
+        game_on=False
+        break
+    elif (len(p2.player_card)==0):
+        print("player 2 out of card player two lost")
+        print("game over")
+        game_on=False
+        break
+
+    else:
+        player_one_fetched=[]
+        player_one_fetched.append(p1._fetch())
+
+        player_two_fetched=[]
+        player_two_fetched.append(p2._fetch())
+
+        war=True
+
+        while war:
+            if (player_one_fetched[-1].value)>(player_two_fetched[-1].value):
+                p1._vangichoo_(player_one_fetched)
+                p1._vangichoo_(player_two_fetched)
+                war=False
+
+
+            elif (player_one_fetched[-1].value)<(player_two_fetched[-1].value):
+                p2._vangichoo_(player_one_fetched)
+                p2._vangichoo_(player_two_fetched)
+                war=False
+
+
+
+            else:
+                if (len(p1.player_card)<5):
+                    print("p1 lost beacuase of less cards")
+                    game_on=False
+                    break
+
+                elif (len(p2.player_card)<5):
+                    print("p2 lost beacuse of less cards")
+                    game_on=False
+                    break
+
+                else:
+                    for i in range(5):
+                        player_one_fetched.append(p1._fetch())
+                        player_two_fetched.append(p2._fetch())
+
+print(round)
+            
+
+                
+
+
+                              
+        
+
 
 
 
